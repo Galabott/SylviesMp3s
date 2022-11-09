3d761092c0c310b9ef6429cb0346be303042236a
Simon <simonaucoin11@gmail.com>
2022-11-09 14:23:32 -0500

    dev-file

---
+++ SylviSQL_DEV-FILE/SylvieSQL/SylvieSQL/User.cs
@@ -0,0 +1,15 @@
+﻿namespace SylvieSQL;
+
+public class User
+{
+    private int id;
+    private string email;
+    private string password;
+
+    public User()
+    {
+        id = 0;
+    }
+
+    public int GetId() { return id; }
+}

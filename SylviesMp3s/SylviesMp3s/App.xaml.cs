cf898928ce0fa021335492703422be4deb431f81
Galabott <G._Botelho@hotmail.com>
2022-12-05 08:47:52 -0500

    mvvm template

--- SylviesMp3s/SylviesMp3s/App.xaml.cs
+++ SylviesMp3s/SylviesMp3s/App.xaml.cs
@@ -6,12 +6,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SylviesMp3s
+namespace $safeprojectname$
 {
     /// <summary>
     /// Interaction logic for App.xaml
+    /// Author : $username$
+    /// Creation Date : $time$
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow win = new MainWindow();
+
+            win.Show();
+        }
     }
 }

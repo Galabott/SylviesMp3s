483d36ef8512a4ec5bed9120f37f360ecaaf6d82
Mathieu Mercier <merciermathieu412@gmail.com>
2022-12-09 09:53:15 -0500

    Ajout boutton navigation entre playlist et album

---
+++ SylviesMp3s/SylviesMP3s/Views/AlbumView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using SylviesMp3s.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SylviesMp3s.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour AlbumView.xaml
+    /// </summary>
+    public partial class AlbumView : UserControl
+    {
+        public AlbumView()
+        {
+            InitializeComponent();
+            DataContext = new AlbumViewModel();
+        }
+    }
+}
